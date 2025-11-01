7cf13acf981c3551272df212f10dd1a86e822165
Tzaodoq Diokno <tzaodoqdiokno@gmail.com>
2025-11-01 16:00:40 +0800

    Added lab10

---
+++ lab10/fill-blanks.js
@@ -0,0 +1,8 @@
+// CODE SNIPPET 11.2 (filled)
+// let name = "_____";
+// let age = _____;
+// console.log("Hi, my name is " + name + " and I am " + _____ + " years old.");
+
+let name = "Tzaodoq";
+let age = 21;
+console.log("Hi, my name is " + name + " and I am " + age + " years old.");
